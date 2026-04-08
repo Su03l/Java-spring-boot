50a55ec4c09dd5c79b38276c727eb97cd2fe4dd3
Suliman <salmanyousef13@gmail.com>
2026-04-09 01:53:46 +0300

    first day done

--- day1-java/day1-java/src/Main.java
+++ day1-java/day1-java/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
-//    the type
+//    the type in java
     int age = 23;
 
     double price = 10.75;
@@ -16,14 +16,13 @@ void main() {
     System.out.println("===================");
 
     // the if statement
-    int score = 85;
 
     if (score >= 90) {
         System.out.println(" The degree : A");
     } else if (score >= 80) {
         System.out.println(" The degree : B");
     } else if (score >= 70) {
-    System.out.println(" The degree : C");
+        System.out.println(" The degree : C");
     } else if (score >= 60) {
         System.out.println(" The degree : D");
     } else {
@@ -69,11 +68,50 @@ void main() {
 
     System.out.println("===================");
 
+    // the while loop
     int counter = 0;
     while (counter < 3) {
         System.out.println("While loop: " + counter);
         counter++;
     }
 
+    System.out.println("===================");
 
+    // the function
+    int result = sum(10, 10);
+    System.out.println("The result is : " + result);
+
+    System.out.println("===================");
+
+
+    // the task 1
+    // about check if the number odd or even
+    int number = 7;
+    if (number % 2 == 0) {
+        System.out.println("The number is even");
+    } else if (number % 2 != 0) {
+        System.out.println("The number is odd");
+    }
+    System.out.println("===================");
+
+    //task 2
+    int result2 = checkDev(5);
+    if (result2 == 1) {
+        System.out.println("The number is divisible by 2");
+    } else if (result2 == 0) {
+        System.out.println("The number is not divisible by 2");
+    }
+
+}
+
+public static int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+public static int checkDev(int num) {
+    if (num / 2 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
