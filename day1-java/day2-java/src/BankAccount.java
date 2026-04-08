9eb4251ad5e3ebd4f950bb37aac586a6e7370315
Suliman <salmanyousef13@gmail.com>
2026-04-09 01:57:07 +0300

    first day done

--- day1-java/day2-java/src/BankAccount.java
+++ day1-java/day2-java/src/BankAccount.java
@@ -4,6 +4,7 @@ public class BankAccount {
     private String accountNumber;
     private double balance;
 
+    // the constructor
     public BankAccount(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
