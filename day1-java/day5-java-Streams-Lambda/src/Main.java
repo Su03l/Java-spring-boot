cb150751eb4a443a4f19e9eb05410f6d41ffcd5f
Suliman <salmanyousef13@gmail.com>
2026-04-10 01:25:58 +0300

    first day done

---
+++ day1-java/day5-java-Streams-Lambda/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // the list of names
+        List<String> names = List.of("Ali", "Sara", "Ahmed", "Fahad");
+
+        System.out.println("Names starting with 'A':");
+
+        names.stream()
+                .filter(name -> name.startsWith("A"))
+                .forEach(name -> System.out.println(name));
+
+        System.out.println("============================");
+
+        List<String> courses = List.of("Java", "Javascript", "React", "PHP", "Python");
+
+        courses.stream()
+                .map(course -> course.toUpperCase())
+                .forEach(course -> System.out.println(course));
+
+        System.out.println("============================");
+        //Task 1
+        List<Integer> ages = List.of(15, 22, 17, 30, 12, 25);
+
+        System.out.println("Ages 18 and over:");
+        ages.stream()
+                .filter(age -> age >= 18)
+                .forEach(age -> System.out.println(age));
+
+        System.out.println("============================");
+
+        //Task 2
+        List<String> tools = List.of("Laravel", "React", "Spring", "Java");
+
+        tools.stream()
+                .map(tool -> "The number of characters in " + tool + " is = " + tool.length())
+                .forEach(text -> System.out.println(text));
+
+        System.out.println("============================");
+    }
+}
