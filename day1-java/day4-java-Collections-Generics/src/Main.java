ff7bc1afd080882df2c63af2fae71101d816894f
Suliman <salmanyousef13@gmail.com>
2026-04-10 01:29:30 +0300

    first day done

--- day1-java/day4-java-Collections-Generics/src/Main.java
+++ day1-java/day4-java-Collections-Generics/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         // Task 1
-
+        // the list for the names
         List<String> names = new ArrayList<>();
 
         names.add("sull");
